ccbbb5c45c33d1e30bbd91a34939871e655d837a
Anatoly Noriega <u202211813@upc.edu.pe>
2024-08-26 21:51:13 -0500

    feat(sales-order): added sales order aggregate root.

--- src/sales/domain/model/sales-order-item.ts
+++ src/sales/domain/model/sales-order-item.ts
@@ -1,5 +1,7 @@
+import {v4 as uuidv4} from 'uuid';
 export class SalesOrderItem{
     private readonly _orderId: string;
+    private readonly _itemId:string;
     private readonly _productId:string;
     private readonly _quantity:number;
     private readonly _unitPrice:number;
@@ -9,6 +11,7 @@ export class SalesOrderItem{
         this._productId = productId;
         this._quantity = quantity;
         this._unitPrice = unitPrice;
+        this._itemId=uuidv4();
     }
     calculateItemPrice=():number=>this._quantity*this._unitPrice;
     get productId():string{
